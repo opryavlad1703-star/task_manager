6d381e34fff7cb5de563c6b0dfe0ebb4ff4078fc
Vladyslav <opria.vlad1703@gmail.com>
2026-09-16 18:22:44 +0300

    Add lab3 report and updated code

--- index.php
+++ index.php
@@ -1,4 +1,39 @@
 <?php
-  echo "<h1>Hello World! Це моя перша PHP-програма.</h1>";
-  phpinfo();
+    $schedule = [
+        4=>[2,"Програмування", "Surkov KU"],
+        5=>[20,"Бази даних", "Knuschyk AV"],
+    ];
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?=$appName?>
+    </h1>
+    <ul>
+        <li> тем: Вивчити основи PHP</li>
+        <li> години на вивчення: 5</li>
+        </li>
+    </ul>
+<?php
+    foreach ($schedule as $key => $value) {
+
+        $text = implode("|", $value);
+            echo "<p>$key.$text</p>";
+    }
+?>
+    <header>
+        <h1>
+            <?=$taskTitle?> </li>
+            <li><?=$taskTimeEstimate?>години</li>
+        </h1>
+    </header>
+</body>
+</html>
